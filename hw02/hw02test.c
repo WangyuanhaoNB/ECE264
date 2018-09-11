ffef2ad7493386c16ba8fb8156c11feaada12dbe
WangyuanhaoNB <43184123+WangyuanhaoNB@users.noreply.github.com>
2018-09-11 14:54:42 -0400

    Add files via upload

---
+++ hw02/hw02test.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<stdbool.h>
+#include "hw02.h"
+#include "hw02.c"
+int main(int argc,char* argv[])
+{
+
+print_integer(88,16,"");
+fputc('\n',stdout);
+return EXIT_SUCCESS;
+}
+
+
+
