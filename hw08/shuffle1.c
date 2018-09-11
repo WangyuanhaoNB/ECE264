782930fcfdb446d9839a9548c004b278f92c704d
WangyuanhaoNB <43184123+WangyuanhaoNB@users.noreply.github.com>
2018-09-11 15:01:22 -0400

    Add files via upload

---
+++ hw08/shuffle1.c
@@ -0,0 +1,15 @@
+#include "shuffle.h"
+
+// You can add more functions to this file.  Helper functions must start with '_'.
+
+void divide(CardDeck orig_deck, CardDeck* upper_deck, CardDeck* lower_deck) {
+}
+
+void interleave(CardDeck upper_deck, CardDeck lower_deck) {
+}
+
+void shuffle(CardDeck orig_deck) {
+}
+
+void repeat_shuffle(CardDeck orig_deck, int k) {
+}
